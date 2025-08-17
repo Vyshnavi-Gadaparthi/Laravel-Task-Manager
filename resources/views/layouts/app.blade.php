6ff109e8dfdc5da4edfba5cd6322a3c1af58c8b0
Vyshnavi-Gadaparthi <152197125+Vyshnavi-Gadaparthi@users.noreply.github.com>
2025-08-16 23:59:44 -0400

    Update app.blade.php to rebrand the taskboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel Task Manager</title>
+    <title>TaskBoard</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
         <div class="container">
-            <a class="navbar-brand" href="{{ route('projects.index') }}">Task Manager</a>
+            <a class="navbar-brand" href="{{ route('projects.index') }}">TaskBoard</a>
         </div>
     </nav>
     <div class="container">
