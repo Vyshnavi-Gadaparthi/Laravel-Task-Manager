1f097ebe15885f1bacd100ba9fc71b3eb258d971
Vyshnavi-Gadaparthi <152197125+Vyshnavi-Gadaparthi@users.noreply.github.com>
2025-08-16 23:18:58 -0400

    Add web routes for projects and tasks

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return redirect()->route('projects.index');
+});
+
+Route::resource('projects', ProjectController::class);
+
+Route::prefix('projects/{project}')->group(function () {
+    Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+});
